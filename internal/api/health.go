c9841d3101efcfd4b4556798703479fff4ce59be
Luis Rodriguez <luis01.ro@gmail.com>
2026-07-28 05:42:09 -0500

    chore: finishing health check

--- internal/api/health.go
+++ internal/api/health.go
@@ -5,7 +5,7 @@ import "net/http"
 func (s *Server) health(w http.ResponseWriter, r *http.Request) {
 	err := s.db.Ping(r.Context())
 	if err != nil {
-		s.log.Err(err).Msg()
+		s.log.Err(err).Msg("database health check failed")
 		w.WriteHeader(http.StatusServiceUnavailable)
 		w.Write([]byte("service unavailable"))
 		return
