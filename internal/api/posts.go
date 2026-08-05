9882dfb2f440104f06229cbd8f164a4c44736a1e
Luis Rodriguez <luis01.ro@gmail.com>
2026-08-04 23:05:41 -0500

    chore: finalizing searching posts endpoin

--- internal/api/posts.go
+++ internal/api/posts.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"errors"
+	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/lucho2027/blog/internal/database/sqlc"
@@ -12,39 +15,73 @@ type PublicPost struct {
 	Excerpt   *string    `json:"excerpt"`
 	Published *time.Time `json:"published"`
 }
-type PostResponse struct {
+type PostsResponse struct {
 	Posts []PublicPost `json:"posts"`
 }
 
+func parsePostsQueryParams(r *http.Request) (sqlc.GetAllPostsParams, error) {
+	offsetStr := r.URL.Query().Get("offset")
+	if offsetStr == "" {
+		offsetStr = "0"
+	}
+	limitStr := r.URL.Query().Get("limit")
+	if limitStr == "" {
+		limitStr = "10"
+	}
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		return sqlc.GetAllPostsParams{}, err
+	}
+	if limit < 1 || limit > 100 {
+		return sqlc.GetAllPostsParams{}, errors.New("limit needs to be a number between 1 and 100")
+	}
+	offset, err := strconv.Atoi(offsetStr)
+	if err != nil {
+		return sqlc.GetAllPostsParams{}, err
+	}
+	if offset < 0 || offset > math.MaxInt32 {
+		return sqlc.GetAllPostsParams{}, errors.New("offset is out of range")
+	}
+	return sqlc.GetAllPostsParams{
+		OffsetVal: int32(offset),
+		LimitVal:  int32(limit),
+	}, nil
+}
+
 func (s *Server) posts(w http.ResponseWriter, r *http.Request) {
-	p := sqlc.GetAllPostsParams{
-		OffsetVal: 0,
-		LimitVal:  10,
+	p, err := parsePostsQueryParams(r)
+	if err != nil {
+		s.log.Err(err).Msg("failed to query posts")
+		if err := writeError(w, http.StatusBadRequest, "invalid pagination parameters"); err != nil {
+			s.log.Err(err).Msg("failed to write error on querying posts")
+		}
+		return
 	}
 
 	posts, err := s.db.Queries().GetAllPosts(r.Context(), p)
 	if err != nil {
 		s.log.Err(err).Msg("failed to get posts")
-		if err := writeError(w, http.StatusInternalServerError, "Internal server error"); err != nil {
+		if err := writeError(w, http.StatusInternalServerError, "internal server error"); err != nil {
 			s.log.Err(err).Msg("failed to write error on posts")
 		}
 		return
 	}
-	resp := PostResponse{
+	resp := PostsResponse{
 		Posts: make([]PublicPost, 0),
 	}
 
-	for _, p := range posts {
+	for _, post := range posts {
 		var excerpt *string
-		if p.Excerpt.Valid {
-			excerpt = &p.Excerpt.String
+		if post.Excerpt.Valid {
+			excerpt = &post.Excerpt.String
 		}
 		var published *time.Time
-		if p.PublishedAt.Valid {
-			published = &p.PublishedAt.Time
+		if post.PublishedAt.Valid {
+			published = &post.PublishedAt.Time
 		}
 		resp.Posts = append(resp.Posts, PublicPost{
-			Title:     p.Title,
+			Title:     post.Title,
 			Excerpt:   excerpt,
 			Published: published,
 		})
